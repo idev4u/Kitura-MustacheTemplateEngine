d14b116bbe474046f76d8d7819ef54bfcbd90b43
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-09-25 10:52:35 +0200

    Prepare package to be ready for swift 4

---
+++ Package@swift-4.0.swift
@@ -0,0 +1,46 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+/**
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import PackageDescription
+
+let package = Package(
+    name: "KituraMustache",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "KituraMustache",
+            targets: ["KituraMustache"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", .upToNextMinor(from: "1.7.2"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "KituraMustache",
+            dependencies: ["KituraTemplateEngine", "GRMustache"]
+        )
+    ]
+)
+
+
+#if !os(Linux) || swift(>=3.1)
+package.dependencies.append(.package(url: "https://github.com/saagarjha/GRMustache.swift.git", from: "2.0.0"))
+#endif
